37f86d1532b2fe61f8d420db69eb38afeb59e97c
twfb <twfb@hotmail.com>
2020-09-03 10:34:08 +0800

    Update nettle.py

--- nettle.py
+++ nettle.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python
-
+import json
 import os
 import time
 from datetime import datetime
 from multiprocessing import Process
 
-# Zhang San: zhangsan@xx.com, zs@xx.com
+TO_IDENTIFIERS = ['zhangsan', 'zs', 'ALL', 'all']
+CC_IDENTIFIERS = ['zhangsan', 'zs', 'Zhang San']
+INTERVAL = 10 * 60 # seconds
+MUTE_INTERVAL = 60 # seconds
+MUTE_COMMANDS = ['mutt', 'tmux']
+HOME_DIR = os.environ['HOME']
+NEW_MAILS_DIR = os.path.join(HOME_DIR, 'Maildir/.inbox/new')
+HISTORY_PATH = os.path.join(HOME_DIR, '.nettle_history')
 
-NAME = 'Zhang San'.lower().split(' ')
-SHORT_NAME = ''.join([i[0] for i in NAME])
-NAME = ''.join(NAME)
-INTERVAL = 30 * 60# seconds
+last_modify = 0
+MUTE_COMMANDS_STR = '|'.join(MUTE_COMMANDS)
 
-NEW_MAILS_DIR = os.path.join(os.environ['HOME'] , 'Maildir/.inbox/new')
-
-def check_in(line):
-    return NAME in line or SHORT_NAME in line
+def check_in(line, identifiers):
+    for i in identifiers:
+        if i in line:
+            return True
+    return False
 
 def get_tty_list():
     who_result = os.popen('who|grep $USER').read().split('\n')
@@ -34,79 +40,123 @@ def get_number(content):
         while line[-1] == ',':
             line += content[i]
             i += 1
-
-        if 'Subject: ' == line[:9]:
+        if 'From: ' == line[:6]:
+            if check_in(line, CC_IDENTIFIERS):
+                return 0, 0
+        elif 'Subject: ' == line[:9]:
             break
-        elif 'To: ' == line[:4] and check_in(line):
+        elif 'To: ' == line[:4] and check_in(line, TO_IDENTIFIERS):
             return 1, 0
         elif 'Cc: '== line[:4]:
-            if check_in(line):
+            if check_in(line, CC_IDENTIFIERS):
                 return 0, 1
             break
     return 0, 0
 
-def set_number(last_modify, unread_mail):
+def set_number(last_modify, unread_email):
     for file_name in os.listdir(NEW_MAILS_DIR):
         mail_path = os.path.join(NEW_MAILS_DIR, file_name)
-        if os.path.getmtime(mail_path) < last_modify - INTERVAL:
+        if os.path.getmtime(mail_path) < last_modify:
             continue
         with open(mail_path, 'r') as f:
             content = f.read().split('\n')
             to, cc = get_number(content)
             if to == 1:
-                unread_mail['to'].add(mail_path)
+                unread_email['to'].append(mail_path)
             elif cc == 1:
-                unread_mail['cc'].add(mail_path)
+                unread_email['cc'].append(mail_path)
 
 def send_message(tty_list, message):
     for i in tty_list:
         os.system('echo "{}" > {}'.format(message, i))
 
-def remove_readed(unread_mail):
-    unread_mail['to'] = set(filter(os.path.exists, unread_mail['to']))
-    unread_mail['cc'] = set(filter(os.path.exists, unread_mail['cc']))
+def remove_readed(unread_email):
+    unread_email['to'] = filter(os.path.exists, unread_email['to'])
+    unread_email['cc'] = filter(os.path.exists, unread_email['cc'])
 
-def check(unread_mail, last_modify):
-    remove_readed(unread_mail)
+def should_mute():
+    return os.popen(
+            'ps -SU $USER|grep -P "({})"|grep -Pv "grep .*-P"'.format(
+                MUTE_COMMANDS_STR
+                )
+            ).read()
+
+def check(unread_email, use_mute=True):
+    global last_modify
+
+    if use_mute and should_mute():
+        return 'mute'
+
     tty_list = get_tty_list()
     if not tty_list:
-        return
+        return 'no_tty'
+
+    remove_readed(unread_email)
 
     if last_modify < os.path.getmtime(NEW_MAILS_DIR):
-        set_number(last_modify, unread_mail)
+        set_number(last_modify, unread_email)
 
-    to_number, cc_number = len(unread_mail['to']), len(unread_mail['cc'])
-    if not (unread_mail['cc'] | unread_mail['to']):
-        return False
+    last_modify = time.time()
+    set_history(unread_email, last_modify)
 
+    to_number, cc_number = len(unread_email['to']), len(unread_email['cc'])
+    if not unread_email['cc'] and not unread_email['to']:
+        return 'none'
     message = '\a\nEmail Notify: To={}; Cc={};'.format(to_number, cc_number)
     send_message(tty_list, message)
-    return True
+    return 'new_email'
+
+def set_history(unread_email, last_modify):
+    with open(HISTORY_PATH, 'w') as f:
+        f.write(json.dumps(dict(
+                unread_email = unread_email,
+                last_modify = last_modify
+        )))
+
+def get_history():
+    if os.path.exists(HISTORY_PATH):
+        with open(HISTORY_PATH) as f:
+            d = json.loads(f.read())
+            return d['unread_email'], d['last_modify']
+    else:
+        return {'cc':[], 'to':[]}, 0
 
 def main():
-    if not NAME:
-        print 'Set NAME first.'
+    global last_modify
+
+    if not TO_IDENTIFIERS and not CC_IDENTIFIERS:
+        print 'Set IDENTIFIERS first.'
         return
 
-    current = datetime.now()
-    unread_mail = {'cc':set(), 'to':set()}
-    last_modify = 0
+    unread_email, last_modify = get_history()
 
-    if not check(unread_mail, last_modify):
+    check_result = check(unread_email, False)
+
+    if check_result == 'none':
         print '\nNo new email.'
 
     while True:
-        if 7 < current.hour < 19:
-            time.sleep(INTERVAL)
-            check(unread_mail, last_modify)
-            last_modify = time.time()
-        else:
+        current = datetime.now()
+        if current.weekday() > 4:
+            time.sleep(12*60*60)
+            continue
+
+        if current.hour < 9 or current.hour > 18:
             time.sleep(60*60)
+            continue
+
+        time.sleep(INTERVAL)
+        check_result = check(unread_email, last_modify)
+        while check_result == 'mute':
+            time.sleep(MUTE_INTERVAL)
+            check_result = check(unread_email, last_modify)
 
 if __name__ == '__main__':
+    pid = str(os.getpid())
     pids = os.popen('pgrep nettle -u $USER').read()
-    pids = [i for i in  pids.split('\n') if i][:-1]
-    for i in sorted(pids):
-        os.system('kill {}'.format(i))
+    for i in pids.split('\n'):
+        if i and i != pid:
+            os.system('kill {}'.format(i))
     Process(target=main).start()
-    os.system("kill -KILL {}".format(os.getpid(), os.getpid()))
+    os.system("kill -KILL {}".format(pid, pid))
+
